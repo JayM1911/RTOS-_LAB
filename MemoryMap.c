1dd968c229f37401b1a7f6bb543cd4041bd77182
JayM1911 <65786671+JayM1911@users.noreply.github.com>
2020-09-11 18:46:19 +0530

    Update MemoryMap.c

--- MemoryMap.c
+++ MemoryMap.c
@@ -1,4 +1,5 @@
 // A)
+
 #include<stdio.h>
 int globe=88;
 int main()
@@ -6,22 +7,28 @@ int main()
 	static int jay=898;
 	return 0;
 }
+
 // B)
+
 #include <stdio.h> 
 int global; 
 int main(void) 
 { 
 	return 0; 
 }
+
 // C)
+
 #include <stdio.h> 
 int global;
 int main(void) 
 { 
 	static int i;
 	return 0; 
-} 
+}
+
 // D)
+
 #include <stdio.h> 
 int global
 int main(void) 
@@ -29,7 +36,9 @@ int main(void)
 	static int i = 100; 
 	return 0; 
 } 
-// E) 
+
+// E)
+
 #include <stdio.h> 
 int global = 10; /* initialized global variable stored in DS*/
 int main(void) 
